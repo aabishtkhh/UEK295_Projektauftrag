4e2d365aa56c402b7ca1c9997f5d70b896efc399
aabishtkhh <aabish.khan@edu.tbz.ch>
2023-04-14 11:57:14 +0200

    Adding project to Github

---
+++ OnlineBookStore/src/test/java/ch/noseryoung/blj/OnlineBookStore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.blj.OnlineBookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
